031bea6104781eb8b5aada23ce4fc309d981b087
Rup-Narayan-Rajbanshi <rswarup03@gmail.com>
2021-09-19 22:41:03 +0545

    chnaged wsgi file

--- project/settings/base.py
+++ project/settings/base.py
@@ -143,10 +143,12 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATIC_ROOT = 'staticfiles'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
+# STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles/')
 
 STATICFILES_DIRS = ()
 
