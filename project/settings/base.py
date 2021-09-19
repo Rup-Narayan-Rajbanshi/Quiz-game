773bddeb8add82a8d5e930c5cbd726d9adefb962
Rup-Narayan-Rajbanshi <rswarup03@gmail.com>
2021-09-19 19:37:00 +0545

    deployment issue

--- project/settings/base.py
+++ project/settings/base.py
@@ -147,8 +147,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     # '/var/www/static/',
