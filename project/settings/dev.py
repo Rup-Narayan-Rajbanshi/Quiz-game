7bfa150966f9c5aec9c5449af8d0e41c6c5e73ba
Rup-Narayan-Rajbanshi <rswarup03@gmail.com>
2021-09-20 13:18:46 +0545

    database name changed

--- project/settings/dev.py
+++ project/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['rup-game-realtime.herokuapp.com']
+ALLOWED_HOSTS = ['rup-game.herokuapp.com']
 
 
 # DATABASES = {
@@ -18,11 +18,11 @@ ALLOWED_HOSTS = ['rup-game-realtime.herokuapp.com']
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dfg7cflmaov4g0',
-        'USER': 'iwnkxrlqaguocz',
-        'HOST': 'ec2-34-228-154-153.compute-1.amazonaws.com',
+        'NAME': 'davtf4n85sjcri',
+        'USER': 'ehgtbainjluitp',
+        'HOST': 'ec2-54-158-247-97.compute-1.amazonaws.com',
         'PORT': 5432,
-        'PASSWORD':'d21e23d60a341d4104e8e128adbe284e8c33a643aee4ff4d14d8e75d53115e10',
+        'PASSWORD':'fbc991a465ae43bc4c8ad77c6d2af8f36d5368916b7305ac13086838754beb31',
     }
 }
 
