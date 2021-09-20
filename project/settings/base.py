9123e2277da2b11fe2010500d67e7b55af23feeb
Rup-Narayan-Rajbanshi <rswarup03@gmail.com>
2021-09-20 09:09:41 +0545

    new abranch commit

--- project/settings/base.py
+++ project/settings/base.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"staticfiles")
 STATIC_URL = '/static/'
-django_heroku.settings(local())
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
